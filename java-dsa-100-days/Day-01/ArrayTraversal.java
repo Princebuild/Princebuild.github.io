e1ce6982e11a10928f130dca0492bdfc61b21aa4
Princebuild <princekumarrfg296@gmail.com>
2026-09-14 22:20:21 +0530

    Create ArrayTraversal.java

---
+++ java-dsa-100-days/Day-01/ArrayTraversal.java
@@ -0,0 +1,13 @@
+public class ArrayTraversal {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+// array.length = array ka size hai
+//  an array is  a collection of elements stored in an ordered way and accessed using an index.
